4a6f471281f3184769babecd77c994b58b985c04
nik <rakot88@gmail.com>
2018-07-29 18:18:09 +0500

    lesson 12 LogAndMes (logs)

--- p0121logandmess/src/main/java/com/example/p0121logandmess/MainActivity.java
+++ p0121logandmess/src/main/java/com/example/p0121logandmess/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.p0121logandmess;
 
+import static com.example.p0121logandmess.R.id.btnOk;
+
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+  private static final String TAG = "myLogs";
+  TextView tvOut;
+  Button btnOk;
+  Button btnCancel;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Log.d(TAG, "Найдем view элементы");
+
+    tvOut = (TextView) findViewById(R.id.tvOut);
+    btnOk = (Button) findViewById(R.id.btnOk);
+    btnCancel = (Button) findViewById(R.id.btnCancel);
+
+    Log.d(TAG, "присваиваем обработчик кнопкам");
+    btnOk.setOnClickListener(this);
+    btnCancel.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View view) {
+    Log.d(TAG, "по id определяем кнопку, вызвавшую этот обработчик");
+    switch (view.getId()) {
+      case R.id.btnOk:
+        // кнопка ОК
+        Log.d(TAG, "кнопка ОК");
+        tvOut.setText("Нажата кнопка ОК");
+        break;
+      case R.id.btnCancel:
+        // кнопка Cancel
+        Log.d(TAG, "кнопка Cancel");
+        tvOut.setText("Нажата кнопка Cancel");
+        break;
+    }
   }
 }
